1e018131704be902b720c240172a2e0de96081e4
Mustafa Khaled <m.khaled@rewe-group.at>
2024-12-27 21:05:08 +0100

    Initial Commit

---
+++ CurrConv/CurrConvApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrConvApp.swift
+//  CurrConv
+//
+//  Created by Mustafa on 27.12.24.
+//
+
+import SwiftUI
+
+@main
+struct CurrConvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CurrConvTests/CurrConvTests.swift
@@ -0,0 +1,17 @@
+//
+//  CurrConvTests.swift
+//  CurrConvTests
+//
+//  Created by Mustafa on 27.12.24.
+//
+
+import Testing
+@testable import CurrConv
+
+struct CurrConvTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
